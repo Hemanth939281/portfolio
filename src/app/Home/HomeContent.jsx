b79448c2e8f55c5aec6db66444a44459ed223791
Hemanth939281 <hkh777650@gmail.com>
2025-02-12 23:56:10 +0530

    added menu and close icons

--- src/app/Home/HomeContent.jsx
+++ src/app/Home/HomeContent.jsx
@@ -10,11 +10,12 @@ import {Grey_Qo, Lato, Mukta, Noto_Sans, Open_Sans} from "next/font/google"
     const notoSans = Noto_Sans({ subsets: ["latin"], weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"] });
     const openSans = Open_Sans({ subsets: ["latin"], weight: ["300", "400", "500", "600", "700", "800"] });
 const HomeContent = ({name, description}) => {
+  
     return (
         <div>
-            <div className="text-white text-lg max-w-full lg:mx-28 mt-18 flex flex-col lg:flex-row gap-16 lg:gap-36 justify-center p-6">
+            <div className="text-white text-lg max-w-full lg:mx-28 lg:mt-[10%] flex flex-col lg:flex-row gap-16 lg:gap-36 justify-center p-6">
             <div className="mt-6 p-4 break-words w-full max-w-1/5">Hi, I'm<span className={`block text-7xl font-bold ${greyQo.className}`}>{name},</span><br/><span className={`block ${lato.className}`}>{description}</span>
-            <div className="flex p-2 gap-16 text-black w-1/2 mt-10">
+            <div className="flex p-2 gap-6 lg:gap-16 text-black w-1/2 mt-10">
             <a href="/MyResume.pdf" target="_blank" className="px-4 py-2 bg-gray-100 hover:bg-gray-300 rounded">See My Resume</a>
             <a href="/contact" className="px-4 py-2 bg-gray-100 hover:bg-gray-300 rounded">Contact me</a>
         </div>
--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,6 +1,30 @@
+"use client"
+
 import Link from "next/link"
+import Image from "next/image"
+import { useEffect, useState } from "react"
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const [isMobile, setIsMobile] = useState(false);
+  
+      useEffect(() => {
+        const checkScreenSize = () => {
+          setIsMobile(window.innerWidth < 768); 
+        };
+    
+        checkScreenSize(); 
+        window.addEventListener("resize", checkScreenSize);
+    
+        return () => window.removeEventListener("resize", checkScreenSize);
+      }, []);
+    
+      const handleClick = () => {
+        if (isMobile) {
+          setIsMenuOpen(false);
+        }
+      };
+    
 
     const links = [
         {name: 'Home', url: "/"},
@@ -8,20 +32,35 @@ const Header = () => {
         {name: 'Projects', url: "/projects"},
         {name: 'Contact me', url: "/contact"}
       ]
+
+      const handleToggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen)
+      }
+
+      
     return (
-        
-      <div className="flex justify-center items-center py-6 mb-10">
-      <div className="m-4 lg:m-auto w-full lg:w-auto text-center flex flex-col lg:flex-row gap-6 text-white text-lg px-4 py-2 rounded-lg bg-red-500 blur-xs shadow-white shadow-md">
+       <div className="relative">
+        <div className={`${isMenuOpen ? "opacity-100 h-full" : "opacity-0 h-0"} lg:opacity-100 lg:flex lg:justify-center lg:items-center py-6 mb-10 transition-opacity duration-300 px-4`}>
+      <div className="relative lg:m-4 lg:m-auto w-full lg:w-auto text-center mt-2 flex flex-col lg:flex-row lg:gap-6 text-white text-lg lg:px-4 py-2 rounded-lg bg-red-500 blur-xs shadow-white shadow-md p-t-[16rem]">
+      {isMenuOpen && 
+           <button onClick={handleToggleMenu} className="mt-2 absolute right-4 rounded lg:hidden mb-4" ><Image src="/close.png" width={20} height={20} alt="menu icon"/></button>
+     }
         {
           links.map((link) => (
 
-            <Link href={link.url} key={link.name} className="block p-2 lg:px-4 py-2 rounded-lg hover:bg-gray-300 hover:text-gray-900 transition duration-300 ease-in-out cursor-pointer">
+            <Link href={link.url} key={link.name} onClick={isMobile ? handleClick : undefined} className="block p-2 lg:px-4 py-2 rounded-lg hover:bg-gray-300 hover:text-gray-900 transition duration-300 ease-in-out cursor-pointer">
               {link.name}
             </Link>
           ))
         }
       </div>
     </div>
+     
+     {!isMenuOpen && 
+           <button onClick={handleToggleMenu} className="absolute top-4 right-4 bg-white rounded lg:hidden" ><Image src="/menu.png" width={30} height={30} alt="menu icon"/></button>
+     }
+
+       </div>
     )
 }
 
